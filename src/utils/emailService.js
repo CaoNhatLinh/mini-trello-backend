0493f90498a928e935ae0786a0d7bd047551f5d3
NhatLinh <caonhatlinh1312@gmai.com>
2025-10-02 01:06:10 +0700

    Update emailService createTransporter add port

--- src/utils/emailService.js
+++ src/utils/emailService.js
@@ -2,10 +2,15 @@ const nodemailer = require('nodemailer');
 // Create email transporter
 const createTransporter = () => {
   return nodemailer.createTransport({
-    service: process.env.EMAIL_SERVICE || 'gmail',
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false, 
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASSWORD
+    },
+    tls: {
+      rejectUnauthorized: false
     }
   });
 };
